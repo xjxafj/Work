f3021e7b9eadd4fa8d36bb9d0b52c36f972d7cea
xjxafj <34830733+xjxafj@users.noreply.github.com>
2020-12-11 14:22:00 +0800

    Add files via upload

---
+++ 代码/c#/类库/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace DNA_SYN.ViewModel
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
